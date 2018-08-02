bc997b3ceb14c3663aecd06e7e7e4b38487c8e9f
Jonathan Luk <jluk87@gmail.com>
2018-08-02 21:12:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,15 +3,14 @@ require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil, name, grade)
     @name = name
     @grade = grade
     @id = id
   end
 
   def self.new_from_db(row)
-    binding.pry
-    Student.new(row[1],row[2], row[0])
+    Student.new(row[0], row[1], row[2])
   end
 
   def self.all
