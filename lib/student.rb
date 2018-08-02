47b9bcff3cb7dd7c8336f44f194e64ce0127e653
Jonathan Luk <jluk87@gmail.com>
2018-08-02 21:09:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
-  def initialize(name, grade, id=nil)
+  def initialize(name:, grade:, id:nil)
     @name = name
     @grade = grade
     @id = id
